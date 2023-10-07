5418096ba25f953704296548e0f6b9e3b5ca935a
Gonzalo <hernandezgonzalo5845@gmail.com>
2023-10-07 19:03:29 -0300

    FIRST COMMIT

---
+++ Charmander.java
@@ -0,0 +1,43 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Charmander here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Charmander extends Criatura{
+     public Charmander(String nombre, boolean imagenEspejada) {
+            super(nombre, 100, new String[] { "Placaje", "- Panzaso -", "- Fabian -", "- Piña -" }, imagenEspejada,
+                    new String[] { "Causa un daño moderado a un enemigo", "Baja 5 de daño", "Te mata de un chivazo", "Baja 3 de daño" });
+        }
+
+    public Charmander(String nombre) {
+        this(nombre, false);
+    }
+
+    public void atacar2(Criatura otro) {
+        atacar1(otro);
+    }
+
+    public boolean puedeRealizarAtaque2En(Criatura otro) {
+        return false;
+    }
+
+    public void atacar3(Criatura otro) {
+        atacar1(otro);
+    }
+
+    public boolean puedeRealizarAtaque3En(Criatura otro) {
+        return false;
+    }
+
+    public void atacar4(Criatura otro) {
+        atacar1(otro);
+    }
+
+    public boolean puedeRealizarAtaque4En(Criatura otro) {
+        return false;
+    }
+   
+}
